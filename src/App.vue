65dfacd38025f0f15b6bff0bd29f1217212c0cf6
hekun <1533431406@qq.com>
2023-06-25 20:34:58 +0800

    625

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>用户登录</h1>
+    <form @submit.prevent="login">
+      <div>
+        <label for="username">用户名：</label>
+        <input id="username" type="text" v-model="username">
+      </div>
+      <div>
+        <label for="password">密码：</label>
+        <input id="password" type="password" v-model="password">
+      </div>
+      <button type="submit">登录</button>
+    </form>
   </div>
 </template>
 
+<style>
+</style>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    async login () {
+     axios({
+      method: 'post',
+      url: 'http://localhost:8081/api/login',
+      data:{
+        username:this.username,
+        password:this.password
+      }
+     }).then(response =>{
+      console.log(response.data)
+     })
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
